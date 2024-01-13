024e2202e4586810a4d8fb26a97bc9173311e040
Prayag Tandon <tandonprayag99@gmail.com>
2024-01-13 20:18:36 +0530

    Scroll behaviour setup done

--- script.js
+++ script.js
@@ -32,3 +32,9 @@ document.addEventListener('keydown', function (e) {
 
 /////////////////////////////////////////
 // IMPLEMENTING SMOOTH SCROLLING
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
+
+btnScrollTo.addEventListener('click', function () {
+  section1.scrollIntoView({ behavior: "smooth" });
+})
